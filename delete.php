d9f1b642b9ab4b0478ee0c7ddacb747ee9d617b5
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-25 16:07:34 +0100

    ça fonctionne

--- delete.php
+++ delete.php
@@ -1,4 +1,23 @@
 <?php
+require 'config.php';
+
+// Démarrage de la session $_SESSION
+session_start();
+
+// récupération de l'id dans l'url
+$id=($_GET['id']);
+
+try{
+    $stmt = $pdo->prepare("DELETE FROM produits WHERE id_produit = ?");
+    $stmt->execute([$id]);
+
+    // Stockage du message dans la session
+    $_SESSION['message'] = "Vous avez bien supprimé votre article !";
+}catch(PDOException $e){
+    echo 'error  est survenue '.$e->getMessage();
+}
+
+
 
 ?>
 
@@ -13,6 +32,17 @@
     <title>Supprimer un produit</title>
 </head>
 <body>
+
+<?php
+// Affichage du message stocké en session
+
+    echo "<p>" . htmlspecialchars($_SESSION['message']) . "</p>";
+    // suppression du message stocké en session
+    unset($_SESSION['message']);
+
+header("Location: index.php");
+
+?>
 
 </body>
 </html>
