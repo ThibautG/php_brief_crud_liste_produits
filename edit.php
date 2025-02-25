f115b6d1bbb54dae0db8ae479df7f8b8d035986b
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-25 16:26:33 +0100

    ajout de la fonctionnalité modifier

--- edit.php
+++ edit.php
@@ -1,4 +1,45 @@
 <?php
+require 'config.php';
+
+// Démarrage de la session $_SESSION
+session_start();
+
+// récupération de l'id dans l'url
+$id=($_GET['id']);
+
+// Vérification de la soumission du formulaire via la method post
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { // $_SERVER est appelée variable super globale
+    // Récupération des données du formulaire
+    //$name = $_POST['nom'];
+    //var_dump($name);
+    //$name = isset($_POST['nom']);
+    $name = isset($_POST['nom']) ? trim($_POST['nom']) : '';
+    $price = isset($_POST['prix']) ? trim($_POST['prix']) : '';
+    $stock = isset($_POST['stock']) ? trim($_POST['stock']) : '';
+    /*var_dump($name);
+    var_dump($price);
+    var_dump($stock);*/
+
+    // Vérification que le champ n'est pas vide
+    if ($name !== '' && $price !== '' && $stock !== '') {
+        try{
+            $stmt = $pdo->prepare("UPDATE produits SET nom_produit = ?, prix_produit = ?, stock_produit = ? WHERE id_produit = ?");
+            $stmt->execute([$name, $price, $stock, $id]);
+
+            // Stockage du message dans la session
+            $_SESSION['message'] = "Vous avez modifié $name pour un prix de $price € à une quantité de $stock";
+        }catch(PDOException $e){
+            echo 'error  est survenue '.$e->getMessage();
+        }
+
+        header("Location: index.php");
+
+        exit();
+    } else {
+        $_SESSION['message'] = "Veuillez remplir tous les champs !";
+    }
+}
+
 
 ?>
 
@@ -13,6 +54,31 @@
     <title>Modifier un produit</title>
 </head>
 <body>
-
+<h1>Modifier un produit</h1>
+<form action="edit.php?id=<?= $id ?>" method="post">
+    <div>
+        <label for="nom">Nom : </label>
+        <input type="text" id="nom" name="nom" required>
+    </div>
+    <div>
+        <label for="prix">Prix : </label>
+        <input type="number" id="prix" name="prix" required>
+    </div>
+    <div>
+        <label for="stock">Stock : </label>
+        <input type="number" id="stock" name="stock" required></input>
+    </div>
+    <div>
+        <button type="submit">Modifier</button>
+    </div>
+</form>
+<?php
+// Affichage du message stocké en session
+if (isset($_SESSION['message'])) {
+    echo "<p>" . htmlspecialchars($_SESSION['message']) . "</p>";
+    // suppression du message stocké en session
+    unset($_SESSION['message']);
+}
+?>
 </body>
 </html>
